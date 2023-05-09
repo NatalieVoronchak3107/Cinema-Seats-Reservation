deeb3138fe24d1884a41a57eff42aa525447cef0
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-05-09 12:37:10 +0300

    adding js

---
+++ main.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.container');
+const couch = document.querySelectorAll('.row .ic_couch:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
