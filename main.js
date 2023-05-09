cbab9a9aa6b93e98eb2f1ea7cb48ebc3bafb81e2
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-05-09 14:55:19 +0300

    updating js

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 const container = document.querySelector('.container');
-const couch = document.querySelectorAll('.row .ic_couch:not(.occupied)');
+const couches = document.querySelectorAll('.row .ic_couch:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
+
+const moviesContainer = document.querySelector('.movies-container');
+const movieElements = moviesContainer.querySelectorAll('.movie');
+let selectedMoviePrice = null;
+
+movieElements.forEach(movie => {
+  movie.addEventListener('click', () => {
+    selectedMoviePrice = parseInt(movie.dataset.price);
+    console.log(`Selected movie price: ${selectedMoviePrice}`);
+    updateSelectedCount(); // Update the count and total when a new movie is selected
+  });
+});
+
+// Updating total price and seats count 
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .ic_couch.selected');
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * selectedMoviePrice;
+}
+
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('ic_couch') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+  }
+
+  updateSelectedCount();
+});
+
