e66c42c88d16973a6040464ef4db4e84b9fc7691
Nataliia Voronchak <natalya.voronchak@gmail.com>
2023-05-10 10:05:15 +0300

    working on saving data to local storage

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ const container = document.querySelector('.container');
 const couches = document.querySelectorAll('.row .ic_couch:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-
 const moviesContainer = document.querySelector('.movies-container');
 const movieElements = moviesContainer.querySelectorAll('.movie');
+
 let selectedMoviePrice = null;
 
-// Saving selected movie index and the price
+// Saving selected movie index and the price in Local Storage
 function saveMovieData (selectedMoviePrice, index) {
     localStorage.setItem('selectedMoviePrice', selectedMoviePrice);
     localStorage.setItem('index', index);
     console.log(`Selected movie price: ${selectedMoviePrice}`);
     console.log(`Movie index: ${index}`);
 }
+
+
 
 // Updating the count and total price when a new movie is selected
 movieElements.forEach((movie, index) => {
@@ -24,7 +26,8 @@ movieElements.forEach((movie, index) => {
   });
 });
 
-// 
+
+//Updating count and total price of selected seats, and store the data in Local Storage
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .ic_couch.selected');
     const selectedSeatsCount = selectedSeats.length;
@@ -48,13 +51,51 @@ function updateSelectedCount() {
     selectedSeatsElement.innerText = seatsDisplayArray.join(', ');
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * selectedMoviePrice;
+}
+
+ /*function updateUI() {
+  const selectedSeatsIndexes = JSON.parse(localStorage.getItem('selectedSeats'));
+  const seats = document.querySelectorAll('.row .ic_couch');
+
+  if (selectedSeatsIndexes !== null && selectedSeatsIndexes.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeatsIndexes.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
   }
+
+  const selectedMovieIndex = localStorage.getItem('index');
+
+  if (selectedMovieIndex !== null) {
+    movieElements[selectedMovieIndex].click();
+  }
+}*/
+
+function updateUI() {
+    const selectedSeatsIndexes = JSON.parse(localStorage.getItem('selectedSeats'));
+    const seats = document.querySelectorAll('.row .ic_couch');
+  
+    if (selectedSeatsIndexes !== null && selectedSeatsIndexes.length > 0) {
+      // Sort the selected seats by their indexes
+      selectedSeatsIndexes.sort().forEach((seatIndex) => {
+        seats[seatIndex].classList.add('selected');
+      });
+    }
+  
+    const selectedMovieIndex = localStorage.getItem('index');
+  
+    if (selectedMovieIndex !== null) {
+      movieElements[selectedMovieIndex].click();
+    }
+  }
+
 
 container.addEventListener('click', (e) => {
   if (e.target.classList.contains('ic_couch') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected');
+    updateSelectedCount();
   }
-
-  updateSelectedCount();
 });
 
+updateUI();
